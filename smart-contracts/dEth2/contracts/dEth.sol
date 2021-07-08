03cc4469eb3ef2ed1027afb878d3d75b52203b4c
Михаил Проказин <bearpro@outlook.com>
2021-07-08 19:49:34 +0300

    Openzeppelin depenency via npm.

--- smart-contracts/dEth2/contracts/dEth.sol
+++ smart-contracts/dEth2/contracts/dEth.sol
@@ -3,8 +3,8 @@
 
 pragma solidity ^0.5.17;
 
-import "../../common.5/openzeppelin/token/ERC20/ERC20Detailed.sol";
-import "../../common.5/openzeppelin/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "./DSMath.sol";
 import "./DSProxy.sol";
 
